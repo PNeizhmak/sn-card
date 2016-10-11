bac20a27764b6583bd4232bee6ac4c97aa7595a0
Pavel Neizhmak <PavelN@playtika.com>
2016-10-11 09:41:26 +0300

    add initial project structure

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * @author Pavel Neizhmak
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("social network business card");
+    }
+}
